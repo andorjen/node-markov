da5db3dad2db45a85f123f5e6a02ea9e2c7e2f42
Jenny Liu <jennyliuye@gmail.com>
2021-09-28 17:40:22 -0700

    add test

--- makeText.js
+++ makeText.js
@@ -1,1 +1,47 @@
 /** Command-line tool to generate Markov text. */
+"use strict";
+const { MarkovMachine } = require('./markov');
+
+const fsP = require('fs/promises')
+const axios = require('axios')
+
+/** function that takes a path to a file */
+
+
+async function cat(path) {
+
+    try {
+        let contents = await fsP.readFile(path, "utf8");
+        let markov = new MarkovMachine(contents)
+        console.log(markov.getText());
+    } catch (err) {
+        console.log(err.stack);
+        process.exit(1);
+
+    }
+};
+
+
+async function webCat(url) {
+    try {
+        let resp = await axios.get(url);
+        // console.log("resp", resp)
+        let markov = new MarkovMachine(resp.data)
+        console.log(markov.getText());
+    } catch (err) {
+        console.log(err.stack);
+        process.exit(1);
+    }
+};
+
+
+
+let path = process.argv[3];
+console.log("path is", path)
+if (path.includes('http://')) {
+    console.log("inside webCat")
+    webCat(path);
+} else {
+    cat(path);
+}
+
--- markov.js
+++ markov.js
@@ -18,19 +18,33 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains(words) {
-    let chain = new Map();
+    let chain = {};
 
     for (let i = 0; i < words.length; i++) {
 
-      if (!chain.has(words[i])) {
-        chain.set(words[i], [words[i + 1] || null]);
+      if (!(words[i] in chain)) {
+        chain[words[i]] = [words[i + 1] || null];
       } else {
-        chain.get(words[i]).push(words[i + 1] || null);
+        chain[words[i]].push(words[i + 1] || null);
       }
     }
     return chain;
   }
 
+
+  // makeChains(words) {
+  //   let chain = new Map();
+
+  //   for (let i = 0; i < words.length; i++) {
+
+  //     if (!chain.has(words[i])) {
+  //       chain.set(words[i], [words[i + 1] || null]);
+  //     } else {
+  //       chain.get(words[i]).push(words[i + 1] || null);
+  //     }
+  //   }
+  //   return chain;
+  // }
 
 
   /** return random text from chains */
@@ -41,27 +55,25 @@ class MarkovMachine {
     //pick random word from list of keys in chain and then 
     // pick a random word from its chain value
     // use that as next key and then find anotehr word and loop
-    let uniqueWords = Array.from(this.chain.keys());
-    
-    let randIdx = Math.floor(Math.random() * uniqueWords.length);
+    let uniqueWords = Object.keys(this.chain);
+
+    let randIdx = Math.floor(Math.random() * uniqueWords.length);  //make a method choice to pick a random item from a given array
 
     let currentWord = uniqueWords[randIdx];
 
     let result = currentWord;
 
-    let wordChoices;
 
-    for (let i = 0; i < numWords; i++){
-      
-      wordChoices = this.chain.get(currentWord);
+    for (let i = 0; i < numWords; i++) {
 
+      let wordChoices = this.chain[currentWord];
       randIdx = Math.floor(Math.random() * wordChoices.length);
-      
       currentWord = wordChoices[randIdx];
 
-      if(currentWord){
-        result += ` ${currentWord}`;
-      }else{
+      if (currentWord) {
+        result += ` ${currentWord}`; // push words into an array, and join at end
+      }// if hit null, pick a new word
+      else {
         randIdx = Math.floor(Math.random() * uniqueWords.length);
         currentWord = uniqueWords[randIdx];
       }
@@ -70,31 +82,56 @@ class MarkovMachine {
   }
 }
 
+// getText(numWords = 100) {
+
+//   //loop num words of times
+//   //pick random word from list of keys in chain and then 
+//   // pick a random word from its chain value
+//   // use that as next key and then find anotehr word and loop
+//   let uniqueWords = Array.from(this.chain.keys());
+
+//   let randIdx = Math.floor(Math.random() * uniqueWords.length);
+
+//   let currentWord = uniqueWords[randIdx];
+
+//   let result = currentWord;
+
+//   let wordChoices;
+
+//   for (let i = 0; i < numWords; i++) {
+
+//     wordChoices = this.chain.get(currentWord);
+
+//     randIdx = Math.floor(Math.random() * wordChoices.length);
+
+//     currentWord = wordChoices[randIdx];
+
+//     if (currentWord) {
+//       result += ` ${currentWord}`;
+//     } else {
+//       randIdx = Math.floor(Math.random() * uniqueWords.length);
+//       currentWord = uniqueWords[randIdx];
+//     }
+//   }
+//   return result += ".";
+// }
+
+module.exports = { MarkovMachine };
 
 
-
-let myMachine = new MarkovMachine(`Four score and seven years ago our fathers brought forth on this continent, a
-new nation, conceived in Liberty, and dedicated to the proposition that all men
-[sic] are created equal.
-
-Now we are engaged in a great civil war, testing whether that nation, or any
-nation so conceived and so dedicated, can long endure. We are met on a great
-battle-field of that war. We have come to dedicate a portion of that field, as
-a final resting place for those who here gave their lives that that nation
-might live. It is altogether fitting and proper that we should do this.
-
-But, in a larger sense, we can not dedicate -- we can not consecrate -- we can
-not hallow -- this ground. The brave men, living and dead, who struggled here,
-have consecrated it, far above our poor power to add or detract. The world will
-little note, nor long remember what we say here, but it can never forget what
-they did here. It is for us the living, rather, to be dedicated here to the
-unfinished work which they who fought here have thus far so nobly advanced. It
-is rather for us to be here dedicated to the great task remaining before us --
-that from these honored dead we take increased devotion to that cause for which
-they gave the last full measure of devotion -- that we here highly resolve that
-these dead shall not have died in vain -- that this nation, under God, shall
-have a new birth of freedom -- and that government of the people, by the
-people, for the people, shall not perish from the earth.
-`)
-// console.log(myMachine.chain)
-console.log(myMachine.getText())
+// let myMachine = new MarkovMachine(`
+// But, in a larger sense, we can not dedicate -- we can not consecrate -- we can
+// not hallow -- this ground. The brave men, living and dead, who struggled here,
+// have consecrated it, far above our poor power to add or detract. The world will
+// little note, nor long remember what we say here, but it can never forget what
+// they did here. It is for us the living, rather, to be dedicated here to the
+// unfinished work which they who fought here have thus far so nobly advanced. It
+// is rather for us to be here dedicated to the great task remaining before us --
+// that from these honored dead we take increased devotion to that cause for which
+// they gave the last full measure of devotion -- that we here highly resolve that
+// these dead shall not have died in vain -- that this nation, under God, shall
+// have a new birth of freedom -- and that government of the people, by the
+// people, for the people, shall not perish from the earth.
+// `)
+// // console.log(myMachine.chain)
+// console.log(myMachine.getText())
